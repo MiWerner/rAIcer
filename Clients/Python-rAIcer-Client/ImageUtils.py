eae8389eda6d4adc03a34732a0d69a4bd871b8f8
Marius Krusen <marius.krusen@student.uni-luebeck.de>
2018-05-04 01:43:31 +0200

    Cleaned up ImageUtils

--- Clients/Python-rAIcer-Client/ImageUtils.py
+++ Clients/Python-rAIcer-Client/ImageUtils.py
@@ -1,7 +1,7 @@
 from Utils import IMG_HEIGHT, IMG_WIDTH, print_debug
+import MatrixOps
 import cv2 as cv
 import numpy as np
-import time
 
 MIN_BALL_RADIUS = 5
 MIN_PRIMARY_COLOR_VALUE_BALL = 100
@@ -48,11 +48,21 @@ def get_ball_position(ID, img):
 
 
 def get_distance_to_center(point):
+    """
+    Returns the Hammington distance of the given point to the window center
+    :param point: the point
+    :return: the Hammington distance of the given point to the window center
+    """
     window_center = np.array([IMG_WIDTH/2, IMG_HEIGHT/2])
     return np.sum(np.abs(np.subtract(window_center, point)))  # Hammington distance is good enough
 
 
 def get_track(img):
+    """
+    Creates and returns a binary image of the track. Also finds track sections and starts the calculation of the racing line
+    :param img: the image
+    :return: a binary image of the track
+    """
     # Set the bounds to detect the black parts of the track and invert it to get the actual track
     lower_bounds_track = np.array([0, 0, 0])
     upper_bounds_track = np.array([10, 10, 10])
@@ -68,38 +78,17 @@ def get_track(img):
     finish_line_coords = cv.findNonZero(finish_line)
     finish_line_center = np.flip(np.mean(finish_line_coords, axis=0)[0], 0)
 
+    # Get the contours of the track
     _, contours, hierarchy = cv.findContours(track, cv.RETR_TREE, cv.CHAIN_APPROX_NONE)
     image = track.copy()
 
-    inner_contour = None
-    outer_contour = None
-    inner_start_index = None
-    outer_start_index = None
-    for c in contours:
-        print_debug("Contour-Length: ", len(c))
-        print_debug("Contour:", c)
-        intersection = multidim_intersect(c, finish_line_coords)
-        if intersection.any():
-            median_intersection = intersection[int(len(intersection)/2)]
-            intersection_index = multidim_indexof(c, median_intersection)
-
-            if inner_start_index is None:
-                inner_contour = c
-                inner_start_index = intersection_index
-            else:
-                if get_distance_to_center(median_intersection) < get_distance_to_center(inner_contour[inner_start_index]):
-                    outer_contour = inner_contour
-                    inner_contour = c
-                    outer_start_index = inner_start_index
-                    inner_start_index = intersection_index
-                else:
-                    outer_contour = c
-                    outer_start_index = intersection_index
+    # Get the inner and outer contour and the indices of the points where they touch the finish line
+    inner_contour, outer_contour, inner_start_index, outer_start_index = _get_inner_and_outer_contour(contours, finish_line_coords)
 
     # Calculate the starting direction based on the center of the actual start line and center of the orange area
-    direction = finish_line_center - convex_combination(inner_contour[inner_start_index][0], outer_contour[outer_start_index][0], 0.5, True)
+    direction = finish_line_center - MatrixOps.convex_combination(inner_contour[inner_start_index][0], outer_contour[outer_start_index][0], 0.5, True)
 
-    # Draw cross sections of the track
+    # Calculate cross sections of the track
     number_of_sections = 50
     sections = np.empty([number_of_sections, 2, 2], dtype=int)
     for i in range(0, number_of_sections):
@@ -111,34 +100,42 @@ def get_track(img):
         sections[i][1] = outer_contour[outer_index][0]
         cv.line(image, tuple(sections[i][0]), tuple(sections[i][1]), (0, 0, 0))
 
-    # image and track seem to be the same image
     cv.imshow("track", image)
     return track  # Not sure what to actually return later, just return anything so this does not get called again
 
 
-def multidim_intersect(arr1, arr2):
-    # Remove third dimension from the arrays
-    arr1 = arr1.reshape(len(arr1), 2)
-    arr2 = arr2.reshape(len(arr2), 2)
-    # Change the inner arrays to tuples to compare them
-    arr1_view = arr1.view([('', arr1.dtype)]*arr1.shape[1])
-    arr2_view = arr2.view([('', arr2.dtype)]*arr2.shape[1])
-    # Find the intersections and return them
-    intersected = np.intersect1d(arr1_view, arr2_view)
-    return intersected.view(arr1.dtype).reshape(-1, arr1.shape[1])
+def _get_inner_and_outer_contour(contours, finish_line_coords):
+    """
+    Finds the contours that touch the start/finish line and returns them as inner and outer contour
+    as well as the index of the point where the start/finish line touches the contour
+    :param contours: all found contours
+    :param finish_line_coords: array containing all coordinates of the finish line
+    :return: the inner and outer contour and the indices of the points where they touch the finish line
+    """
+    inner_contour = None
+    outer_contour = None
+    inner_start_index = None
+    outer_start_index = None
+    for c in contours:
+        # Find all intersecting points of finish line and contour
+        intersection = MatrixOps.multidim_intersect(c, finish_line_coords)
+        if intersection.any():
+            # If there is ar intersecting points, get the median point and find their index
+            median_intersection = intersection[int(len(intersection) / 2)]
+            intersection_index = MatrixOps.multidim_indexof(c, median_intersection)
 
-
-def multidim_indexof(array, element):
-    # Maybe improve performance later? The numpy variants do not seem to work though
-    for i in range(0, len(array)):
-        if np.array_equal(array[i][0], element):
-            return i
-    return -1
-
-
-def convex_combination(point1, point2, factor=0.5, flip=False):
-    result = point1*factor + point2*(1-factor)
-    return np.flip(result, 0) if flip else result
-
-
-
+            # Determine if the found point belongs to the inner or outer contour and set it accordingly
+            if inner_start_index is None:
+                inner_contour = c
+                inner_start_index = intersection_index
+            else:
+                if get_distance_to_center(median_intersection) < get_distance_to_center(
+                        inner_contour[inner_start_index]):
+                    outer_contour = inner_contour
+                    inner_contour = c
+                    outer_start_index = inner_start_index
+                    inner_start_index = intersection_index
+                else:
+                    outer_contour = c
+                    outer_start_index = intersection_index
+    return inner_contour, outer_contour, inner_start_index, outer_start_index

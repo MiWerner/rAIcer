c54c5c3ae60faf034569b762be128b5dc15f08e7
Unknown <franz.werner@student.uni-luebeck.de>
2018-05-23 12:18:34 +0200

    Added Config file discription

---
+++ Clients/Python-rAIcer-Client/res/configs/NEAT-Python-Configfile-Documentation-Dateien/readthedocs-data.js
@@ -0,0 +1,14 @@
+  var READTHEDOCS_DATA = {
+    project: "neat-python",
+    version: "latest",
+    language: "en",
+    subprojects: {},
+    canonical_url: "http://neat-python.readthedocs.io/en/latest/",
+    theme: "sphinx_rtd_theme",
+    builder: "sphinx",
+    docroot: "/docs/",
+    source_suffix: ".rst",
+    api_host: "https://readthedocs.org",
+    commit: "15e910ce"
+  };
+  

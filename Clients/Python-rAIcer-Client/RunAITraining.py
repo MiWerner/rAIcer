07cc33eb02035123d34529a1edffae4a1ef166ca
Unknown <franz.werner@student.uni-luebeck.de>
2018-05-23 12:42:05 +0200

    Imporved Terminal arguments

--- Clients/Python-rAIcer-Client/RunAITraining.py
+++ Clients/Python-rAIcer-Client/RunAITraining.py
@@ -7,7 +7,9 @@ parser.add_argument("--speed", action="store_true", default=False, help="Enables
 parser.add_argument("--ballpos", action="store_true", default=False, help="Enables ballpos features")
 parser.add_argument("--num_cp", type=int, default=3, help="Sets the number of Checkpoints in feature vector")
 parser.add_argument("--config", type=str, default=None, help="filename of the configfile in config folder")
-parser.add_argument("--restore", type=str, default=None, help="If given the population is restored from the given file")
+parser.add_argument("--restore", action="store_true", default=None, help="If set the population is restored from the file defined by restore_folder and checkpoint_id")
+parser.add_argument("--restore_folder", type=str, default=None, help="folder name of the run to restore")
+parser.add_argument("--checkpoint_id", type=int, default=None, help="id of the checkpoint used to restore run")
 parser.add_argument("--num_gen", type=int, default=100, help="The number of generations to run")
 
 args = parser.parse_args()
@@ -20,6 +22,7 @@ update_feature_parameters(hv_dist=args.hv_dists,
                           num_cp_features=args.num_cp)
 
 from AITraining import AITrainer
-AITrainer.run_training(N=args.num_gen, path_to_config=args.config, path_to_restore=args.restore)
+AITrainer.run_training(N=args.num_gen, path_to_config=args.config,
+                       restore=args.restore, restore_folder=args.restore_folder, restore_checkpoint=args.checkpoint_id)
 
 
--- Clients/Python-rAIcer-Client/runAITraining.sh
+++ Clients/Python-rAIcer-Client/runAITraining.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-python3 RunAITraining.py --speed --num_cp 1 --num_gen 50 --config neat_config_no_hidden --restore 2018-05-20_23-21-05/checkpoints/checkpoint-22
+python3 RunAITraining.py --speed --num_cp 1 --num_gen 50 --config neat_config_no_hidden --restore --restore_folder 2018-05-20_23-21-05 --checkpoint_id 22
